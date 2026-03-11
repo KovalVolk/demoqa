7bd81ef54c1e76dba5ff13ee85c44f873bc5255c
KovalVolk <kovalvolkova.e@gmail.com>
2026-03-11 11:52:09 +0300

    first commit

---
+++ pages/accordion.py
@@ -0,0 +1,14 @@
+from components.components import WebElement
+from pages.base_page import BasePage
+
+class Accordion(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/accordian'
+        super().__init__(driver, self.base_url)
+
+        self.section_content = WebElement(driver, '#section1Content > p')
+        self.section_heading = WebElement(driver, '#section1Heading')
+        self.section1_content = WebElement(driver, 'p:nth-child(1)')
+        self.section2_content = WebElement(driver, 'p:nth-child(2)')
+        self.section3_content = WebElement(driver, 'p')
---
+++ tests/test_hw/test_visible_hw.py
@@ -0,0 +1,31 @@
+import time
+from pages.accordion import Accordion
+
+def test_page_accordion(browser):
+    accord_page = Accordion(browser)
+
+    accord_page.visit()
+    assert accord_page.section_content.visible()
+
+    accord_page.section_heading.click()
+    time.sleep(2)
+    assert not accord_page.section_content.visible()
+
+def test_visible_accordion_default(browser):
+    accord_page = Accordion(browser)
+    accord_page.visit()
+
+    assert accord_page.section1_content.visible()
+    accord_page.section1_content.click()
+    time.sleep(2)
+    assert not accord_page.section1_content.visible()
+
+    assert accord_page.section2_content.visible()
+    accord_page.section2_content.click()
+    time.sleep(2)
+    assert not accord_page.section2_content.visible()
+
+    assert accord_page.section3_content.visible()
+    accord_page.section3_content.click()
+    time.sleep(2)
+    assert not accord_page.section3_content.visible()
